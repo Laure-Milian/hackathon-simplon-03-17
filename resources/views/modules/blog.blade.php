aab0400ceb6bd52bad03498b772f1dca4d6d6543
marineco <marinecolonge@gmail.com>
2017-03-20 23:56:06 +0100

    création fichier css spécifique au module blog importé dans le app.scss -> enrichissement mise en forme

--- resources/views/modules/blog.blade.php
+++ resources/views/modules/blog.blade.php
@@ -3,26 +3,38 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Blog</title>
+	<link rel="stylesheet" href="{{mix('/css/app.css')}}">
 </head>
 <body>
-	<h2>Les derniers articles du blog</h2>
-
-	<div class="row">
-
-		@foreach ($articles as $article)
+	<div class="content">
+		<h2 class="title">Les dernières actus du blog</h2>
 		
-		<div class="col-xs-6 col-md-4">
-			<h3>{{ $article[0] }}</h3>
-			<p>{{ $article[1] }}</p>
-			<p>{{ $article[2] }}</p>
-			<img src="{{ $article[3] }}"/>
-			<p>{{ $article[4] }}</p>
+		<div class="row">
+		
+			@foreach ($articles as $article)
+			
+			<div class="col-xs-6 col-md-4">
+				<h3>{{ $article[0] }}</h3>
+				
+				<div class="details">
+					<p>Publié le {{ $article[1] }}</p> 
+					<p>&nbsp; par {{ $article[2] }}</p>
+				</div>
+				
+				<div class="article">
+					<div>
+						<img src="{{ $article[3] }}"/>
+					</div>
+					<div>
+						<p>{{ $article[4] }}</p>
+					</div>
+				</div>
+			</div>
+		
+			@endforeach
+			
 		</div>
-
-		@endforeach
-		
 	</div>
-
 
 </body>
 </html>
