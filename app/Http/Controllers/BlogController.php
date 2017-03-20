de1e168cce1ba034c9ac40066a9c1bff66aa3d9f
marineco <marinecolonge@gmail.com>
2017-03-20 16:25:13 +0100

    récupération date de publication

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -27,23 +27,55 @@ class BlogController extends Controller
 		}
 
 
+		//Get publication date
+		$url = "http://www.groupe-adonis.fr/blog/feed";
+		$rss = simplexml_load_file($url);
+		$i = 0;
+		foreach ($rss->channel->item as $item){
+			$resultPub[$i++] = $item->pubDate;
+		}
+
+		$datetime1 = date_create($resultPub[0]);
+		$date1 = date_format($datetime1, 'd M Y, H\hi');
+
+		$datetime2 = date_create($resultPub[1]);
+		$date2 = date_format($datetime2, 'd M Y, H\hi');
+
+		$datetime3 = date_create($resultPub[2]);
+		$date3 = date_format($datetime3, 'd M Y, H\hi');
+
+
+
+		//Get creator
+		$creator = $xpath->query("//span[contains(@class, 'username')]");
+		$i = 0;
+		foreach ($creator as $exemple) {
+			$resultCrea[$i++] = $exemple->nodeValue;
+		}
+
 		//Display
-		echo 'Dernier article :';
+		echo 'Article 3:';
 		echo '<ul>';
-			echo '<li>'.$resultTitle[0].'</li>';
-			echo '<li>'.$resultDes[0].'description 1er article </li>';
+		echo '<li>'.$resultTitle[0].'</li>';
+		echo '<li>'.$date1.'</li>';
+		echo '<li>'.$resultCrea[0].'</li>';
+		echo '<li>'.$resultDes[0].'</li>';
 		echo '</ul>';
 
-		echo 'Avant dernier article :';
+		echo 'Article 2:';
 		echo '<ul>';
-			echo '<li>'.$resultTitle[1].'</li>';
-			echo '<li>'.$resultDes[1].'description 2e article</li>';
+		echo '<li>'.$resultTitle[1].'</li>';
+		echo '<li>'.$date2.'</li>';
+		echo '<li>'.$resultCrea[1].'</li>';
+		echo '<li>'.$resultDes[1].'</li>';
 		echo '</ul>';
 
-		echo 'Avant avant dernier article :';
+		echo 'Article 1:';
 		echo '<ul>';
-			echo '<li>'.$resultTitle[2].'</li>';
-			echo '<li>'.$resultDes[2].'description 3e article</li>';
+		echo '<li>'.$resultTitle[2].'</li>';
+		echo '<li>'.$date3.'</li>';
+		echo '<li>'.$resultCrea[2].'</li>';
+		echo '<li>'.$resultDes[2].'</li>';
 		echo '</ul>';
 
 		return view('modules.blog');
