ff040b0be0aede0341ee2b066c61651927f3e035
marineco <marinecolonge@gmail.com>
2017-03-20 15:24:46 +0100

    modif récupération xpath: title + description check

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,23 +9,41 @@ class BlogController extends Controller
 	public function getItem() {
 
 		$html = new \DOMDocument();
-		@$html->loadHtmlFile('http://www.groupe-adonis.fr/blog/feed');
+		@$html->loadHtmlFile('http://www.groupe-adonis.fr/blog');
 		$xpath = new \DOMXPath($html);
-		$domExemple = $xpath->query("//item");
 
-		echo '<ul>';
-
+		//Get title
+		$title = $xpath->query("//div[contains(@class, 'article_title')]");
 		$i = 0;
-		foreach ($domExemple as $exemple) {
-			$result[$i++] = $exemple->nodeValue;
-			
+		foreach ($title as $exemple) {
+			$resultTitle[$i++] = $exemple->nodeValue;
 		}
-			echo '<li>'.$result[0].'</li>';
-			echo '<li>'.$result[1].'</li>';
-			echo '<li>'.$result[2].'</li>';
+		
+		//Get description
+		$description = $html->getElementsByTagName('p');
+		$i = 0;
+		foreach ($description as $exemple) {
+			$resultDes[$i++] = $exemple->nodeValue;
+		}
 
 
+		//Display
+		echo 'Dernier article :';
+		echo '<ul>';
+			echo '<li>'.$resultTitle[0].'</li>';
+			echo '<li>'.$resultDes[0].'description 1er article </li>';
+		echo '</ul>';
 
+		echo 'Avant dernier article :';
+		echo '<ul>';
+			echo '<li>'.$resultTitle[1].'</li>';
+			echo '<li>'.$resultDes[1].'description 2e article</li>';
+		echo '</ul>';
+
+		echo 'Avant avant dernier article :';
+		echo '<ul>';
+			echo '<li>'.$resultTitle[2].'</li>';
+			echo '<li>'.$resultDes[2].'description 3e article</li>';
 		echo '</ul>';
 
 		return view('modules.blog');
