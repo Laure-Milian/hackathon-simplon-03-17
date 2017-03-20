24d1b2cf4660fb7a943c62ea234f669c0753fc13
Axel <lezinaxel@gmail.com>
2017-03-20 16:44:57 +0100

    Affichage des profs si la date de naissance correspond à la date du jour

--- app/Http/Controllers/ProfController.php
+++ app/Http/Controllers/ProfController.php
@@ -9,6 +9,13 @@ class ProfController extends Controller
 {
   public function getDates(){
     $profs = Prof::all();
+    $dateNow = date('m-d');
+    foreach ($profs as $value) {
+      $birthday = substr($value->dateNaissance, 5);
+      if ($dateNow == $birthday) {
+        echo $value->dateNaissance.$value->prenom;
+      }
+    }
     return view('modules.prof', compact('profs'));
   }
 }
