58151c88e6bded4226c6f496dd34927be3006597
Juliane Blier <blier.juliane@gmail.com>
2017-03-20 13:05:06 +0100

    Ajout de la page de display

---
+++ resources/views/display/modules.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Affichage des Modules</title>
+  </head>
+  <body>
+
+    <div class="module">
+      @include('modules.module1')
+    </div>
+    <div class="module">
+      @include('modules.module2')
+    </div>
+    <div class="module">
+      @include('modules.module3')
+    </div>
+    <div class="module">
+      @include('modules.module4')
+    </div>
+
+  </body>
+</html>
