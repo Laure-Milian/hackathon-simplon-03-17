f71ee9a18b9f70ff8959b7f4e3e4b8072d55fd20
simplon <laure.milian@gmail.com>
2017-03-21 14:20:02 +0100

    recupere les evenements de la timeline, 60, dans l'odre supérieurs à la date du jour

--- app/Http/Controllers/PlanningController.php
+++ app/Http/Controllers/PlanningController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Adonis_tl;
+use Carbon\Carbon;
 
 class PlanningController extends Controller
 {
     
     public function getTimeline() {
     	echo "hey";
-    	$tl = Adonis_tl::all();
-    	var_dump($tl);
+    	$today = substr(Carbon::now(), 0, 10);
+    	$events = Adonis_tl::where('laDate', '>=', $today)
+		->orderBy('laDate', 'asc')
+		->take(60)
+		->get();
+    	foreach ($events as $event) {
+    		if ($event->laDate >= $today)
+    			echo '<div>' . $event->laDate . '</div>';
+    	}
     	return view('modules/planning');
     }
 
