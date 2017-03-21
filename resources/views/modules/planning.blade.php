6ca0b06e076d7e28cbc57d62c8574164a3941a17
simplon <laure.milian@gmail.com>
2017-03-21 11:00:20 +0100

    ajout bootstrap et création vue

---
+++ resources/views/modules/planning.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Planning Adonis</title>
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="panel panel-default">
+		<div class="panel-heading">Panel heading</div>
+		<table class="table">
+			<tr>
+				<th>Jour</th>
+				<th>Début</th>
+				<th>Fin</th>
+				<th>Groupe</th>
+				<th>Formateur</th>
+				<th>Salle</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+			</tr>
+		</table>
+</div>
+</body>
+</html>
