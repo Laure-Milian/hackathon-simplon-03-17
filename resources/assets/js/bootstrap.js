ddd553a544d6ebe1a992b5f03ed4623e69c35969
Juliane Blier <blier.juliane@gmail.com>
2017-03-28 12:14:29 +0200

    Preparation for heroku

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -4,7 +4,7 @@
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
-
-window.$ = window.jQuery = require('jquery');
-
-require('bootstrap-sass');
+//
+// window.$ = window.jQuery = require('jquery');
+//
+// require('bootstrap-sass');
--- resources/views/display/modules.blade.php
+++ resources/views/display/modules.blade.php
@@ -22,6 +22,7 @@
       @include('modules.anniversaire')
     </div>
 
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript">
       var data = {!! json_encode($configCentre) !!};
     </script>
